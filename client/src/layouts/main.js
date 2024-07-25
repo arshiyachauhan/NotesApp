faaba71083432bd18cc6f218706697f92a738ed8
Arshiya Chauhan <119539567+arshiyachauhan@users.noreply.github.com>
2024-07-26 04:39:19 +0530

    Updated

--- client/src/layouts/main.js
+++ client/src/layouts/main.js
@@ -1,11 +1,21 @@
-import React, {Suspense} from "react";
+import React, {Suspense, useEffect} from "react";
 import Loader from "../components/shared/loader";
 import styles from "./layout.module.scss";
 import { Outlet } from "react-router-dom";
 import SideBar from "../components/shared/sidebar";
 import Navbar from "../components/shared/navbar";
+import utils from '../utils/localStorage';
+import { useNavigate } from 'react-router-dom';
 
 function Main(){
+    const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = utils.getFromLocalStorage('auth_key');
+    if(!token) {
+      navigate('/');
+    }
+  });
     return (
         //Suspense helps to show something when application is in routing face
         <Suspense fallback={<Loader/>}>
