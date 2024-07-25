0043b93f55cb117a70b0953e989d0d098d531ef3
Arshiya Chauhan <119539567+arshiyachauhan@users.noreply.github.com>
2024-07-26 01:08:09 +0530

    Create index.js

---
+++ client/src/components/shared/brand/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./brand.module.scss";
+import BrandLightImg from "../../../assets/brand-light.svg";
+import BrandDarkImg from "../../../assets/brand-dark.svg";
+function BrandLogo(props){
+    const {logoOnly, type= "light", className}= props;
+    return(
+        <article className={`${styles.brand} ${className}`}>
+            <img src={type==="light"?BrandLightImg : BrandDarkImg} alt="brand-logo"/>
+            {!logoOnly ? (
+                <h1>Note.<span>me</span></h1>
+            ): null}
+            
+        </article>
+    )
+}
+export default BrandLogo;
