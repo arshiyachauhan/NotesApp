38656fe769594e2a9b1fc0d7aca808a635d8636f
Arshiya Chauhan <119539567+arshiyachauhan@users.noreply.github.com>
2024-07-26 00:55:26 +0530

    Create index.js

---
+++ client/src/components/atoms/button/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from'./button.module.scss';
+import {Icon} from '@iconify/react'
+function Button(props){
+    const {text, icon, handleClick, className, isDisabled}=props;
+    return (
+        <article className={`${styles.button} ${className} `} onClick={handleClick} disabled={isDisabled}>
+                <Icon icon={icon} />
+                <h3>{text}</h3>
+            </article>
+    );
+}
+export default Button;
